074c3ba3b3e8238ce52711eec499ae6e7d705bc3
Jakub Białkowski <jakub.bialkowski@fi.abb.com>
2019-04-16 17:02:22 +0200

    Working script which returns number of pages with car on otomoto site ant then returns their prices

--- Ex17.py
+++ Ex17.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def create_soup(web_link):
-    req = requests.get(Link).text
+    req = requests.get(web_link).text
     soup = BeautifulSoup(req, "lxml")
     return soup
 
@@ -18,10 +18,21 @@ def generate_link(page_number):
     return link
 
 
-Link = "https://www.otomoto.pl/osobowe/?search%5Bnew_used%5D=on"
-otomoto_soup = create_soup(Link)
+def print_price(sub_site_number):
+    sub_page_link = generate_link(sub_site_number)
+    sub_page_soup = create_soup(sub_page_link)
+    prices = sub_page_soup.find_all("span", "offer-price__number")
+
+    for car in prices:
+        print(car.text)
+
+
+link = "https://www.otomoto.pl/osobowe/?search%5Bnew_used%5D=on"
+otomoto_soup = create_soup(link)
 number_of_sub_pages = number_of_all_subpages(otomoto_soup)
 
-
-print(generate_link(number_of_sub_pages))
 print(number_of_sub_pages)
+
+for i in range(1, int(number_of_sub_pages)):
+    print_price(str(i))
+
