e0cbdcebe48fab5aade3ed2e25ef32427ef33d12
Jakub Białkowski <jakub.bialkowski@fi.abb.com>
2019-04-18 08:38:10 +0200

    added parser

--- Ex17.py
+++ Ex17.py
@@ -61,7 +61,7 @@ def get_car_info(sub_site_number):
     for i in range(0, len(info)):
         num = i % 4
         if num == 0:
-            year.append(info[i].text)
+            year.append(info[i].text.replace(" ", ""))
         elif num == 1:
             mileage.append(info[i].text)
         elif num == 2:
@@ -73,64 +73,68 @@ def get_car_info(sub_site_number):
     return inf
 
 
+
+
+def list_cars(site_nr):
+
+    prices = get_prices(site_nr)
+    # print(prices)
+
+    cars = get_cars(site_nr)
+    # print(cars)
+
+    info = get_car_info(site_nr)
+    # print(info)
+
+    cars_all = []
+
+    file = open("auta.txt", "a")
+
+    if (
+            len(prices) == len(cars) and
+            len(prices) == len(info[0]) and
+            len(prices) == len(info[1]) and
+            len(prices) == len(info[2]) and
+            len(prices) == len(info[3])
+    ):
+
+        delimeter = ";"
+
+        for i in range(len(prices)):
+
+            try:
+                car = [cars[i].text.replace(" ", "").replace("\n", ""), prices[i].text.replace(" ", "").replace("\n", ""), info[0][i].replace("\n", ""), info[1][i].replace("\n", ""), info[2][i].replace("\n", ""), info[3][i].replace("\n", "")]
+                cars_all.append(car)
+                file.write(car[0] + delimeter +car[1] + delimeter +car[2] + delimeter +car[3] + delimeter +car[4] + delimeter +car[5] + "\n")
+            except Exception as err:
+                print("EXCEPTION:", err)
+                print(len(prices))
+                print(len(cars))
+                print(len(info[0]))
+                print(len(info[1]))
+                print(len(info[2]))
+                print(len(info[3]))
+    else:
+        print("failure")
+        print(len(prices))
+        print(len(cars))
+        print(len(info[0]))
+        print(len(info[1]))
+        print(len(info[2]))
+        print(len(info[3]))
+
+    #print(cars_all[0])
+
+    file.close()
+    return cars_all
+
+
+
 link = "https://www.otomoto.pl/osobowe/?search%5Bnew_used%5D=on"
+site_nr = 2
 
 number_of_pages = get_number_of_sites(link)
 # print(number_of_pages)
 
-prices = get_prices(2)
-# print(prices)
-
-cars = get_cars(2)
-# print(cars)
-
-info = get_car_info(2)
-# print(info)
-
-cars_all = []
-
-if (
-        len(prices) == len(cars) and
-        len(prices) == len(info[0]) and
-        len(prices) == len(info[1]) and
-        len(prices) == len(info[2]) and
-        len(prices) == len(info[3])
-    ):
-
-    for i in range(len(prices)):
-
-        try:
-            car = [cars[i].text, prices[i].text, info[0][i], info[1][i], info[2][i], info[3][i]]
-            cars_all.append(car)
-
-        except Exception as err:
-            print(len(prices))
-            print(len(cars))
-            print(len(info[0]))
-            print(len(info[1]))
-            print(len(info[2]))
-            print(len(info[3]))
-
-else:
-    print("failure")
-    print(len(prices))
-    print(len(cars))
-    print(len(info[0]))
-    print(len(info[1]))
-    print(len(info[2]))
-    print(len(info[3]))
-
-print(cars_all)
-
-
-
-
-# print(info[0][1])
-# print(info[1][2])
-# print(info[2][3])
-# print(info[3][4])
-
-
-# print(len(prices))
-# print(len(cars))
-# print(len(info[0]))
+for i in range(1, 10):
+    list_cars(i)
